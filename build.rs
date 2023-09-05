499a0b510b88bb36acac3211010b10b30df68949
HuangWei <market@zuiyue.com>
2023-09-05 12:24:04 +0800

    remove ico

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("res/daemon.ico");
-        res.compile().unwrap();
-    }
-}
